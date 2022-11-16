decda5fca7dc83ff81beb6d166eda0a9fccd620d
DNBY <lucaspb710@protonmail.com>
2022-11-16 20:29:15 -0300

    primeiro commit Versão de pc que roda em python na linha de comando
    falta fazer interface para android

---
+++ login.py
@@ -0,0 +1,76 @@
+
+from requests import Session, Request
+import requests
+from bs4 import BeautifulSoup
+from getpass import getpass
+from quickstart import add_event
+import quickstart
+
+s = requests.Session()
+
+r = s.get("https://sigaa.ufpb.br/sigaa/logon.jsf")
+soup = BeautifulSoup(r.text, "lxml")
+j_id = soup.find("input", {"id": "javax.faces.ViewState"})["value"]
+
+usuario = input("Seu usuario do sigaa: ")
+senha  = getpass("Senha: ")
+
+dados = {
+    "form": "form",
+    "form:width":	"1920",
+    "form:height": "1080",
+    "form:login": usuario,
+    "form:senha": senha,
+    "form:entrar": "Entrar",
+    "javax.faces.ViewState":	j_id
+}
+
+head =  {
+
+    "Host": "sigaa.ufpb.br",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Origin": "https://sigaa.ufpb.br",
+    "Connection": "keep-alive",
+    "Referer": "https://sigaa.ufpb.br/sigaa/logon.jsf",
+    "Upgrade-Insecure-Requests": "1",
+}
+
+
+s.headers = head
+resp = s.post("https://sigaa.ufpb.br/sigaa/logon.jsf", data=dados)
+
+lel = s.get("https://sigaa.ufpb.br/sigaa/portais/discente/beta/discente.jsf")
+
+if (lel.url == "https://sigaa.ufpb.br/sigaa/portais/discente/beta/discente.jsf"):
+    print("login feito com sucesso")
+else:
+    print("Senha ou usuarion invalidos, tente novamente")
+
+f = open("lel", "w")
+f.write(lel.text)
+
+soup = BeautifulSoup(lel.text, "lxml")
+turmas = soup.find("table", {"class": "minhas-turmas"})
+trs = turmas.find_all("tr")
+
+for i in range(1, len(trs), 1):
+    try:
+        materia_soup = BeautifulSoup(str(trs[i]), "lxml")
+        materia = materia_soup.find("a").text
+        info = materia_soup.find("div", {"class": "info"}).text
+        dia = 0
+        mes = 0
+        for j in range(len(info)):
+            if (info[j] == "/"):
+                dia = int(info[j-2]+info[j-1])
+                mes = int(info[j+1]+info[j+2])
+
+        print(info)
+        print(str(dia)+"/"+str(mes))
+        print(materia)
+        add_event(dia, mes, materia)
+    except:
+        pass
+
+
+
---
+++ quickstart.py
@@ -0,0 +1,64 @@
+from __future__ import print_function
+
+import datetime
+import os.path
+import datetime
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+
+
+"""Shows basic usage of the Google Calendar API.
+Prints the start and name of the next 10 events on the user's calendar.
+"""
+
+def add_event(dia, mes, materia):
+
+    SCOPES = ['https://www.googleapis.com/auth/calendar', 'https://www.googleapis.com/auth/calendar.events']
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('calendar', 'v3', credentials=creds)
+
+    except HttpError as error:
+        print('An error occurred: %s' % error)
+
+    print(str(datetime.date.today().year)+"-"+str(mes)+"-"+str(dia)+"T8:00:00",)
+
+    event = {
+        'summary': materia,
+        'description': "prova de "+materia.lower(),
+        'start':{
+            'dateTime': str(datetime.date.today().year)+"-"+str(mes)+"-"+str(dia)+"T8:00:00",
+            'timeZone': "America/Recife"
+        },
+        'end':{
+            # 'dateTime': "2022-11-14T12:30:00",
+            # 'dateTime': end,
+            'dateTime': str(datetime.date.today().year)+"-"+str(mes)+"-"+str(dia)+"T22:00:00",
+            'timeZone': "America/Recife"
+        }
+    }
+
+    event = service.events().insert(calendarId="primary", body=event).execute()
