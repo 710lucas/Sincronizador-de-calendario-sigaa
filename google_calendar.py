03d4a935c273b0b995f4186e7638d3a55933a3a9
DNBY <lucaspb710@protonmail.com>
2022-11-16 23:42:33 -0300

    impedindo que eventos sejam adicionados mais de uma vez

--- google_calendar.py
+++ google_calendar.py
@@ -19,7 +19,7 @@ Prints the start and name of the next 10 events on the user's calendar.
 
 def add_event(dia, mes, materia):
 
-    SCOPES = ['https://www.googleapis.com/auth/calendar', 'https://www.googleapis.com/auth/calendar.events']
+    SCOPES = ['https://www.googleapis.com/auth/calendar', 'https://www.googleapis.com/auth/calendar.events', 'https://www.googleapis.com/auth/calendar.readonly']
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -62,3 +62,32 @@ def add_event(dia, mes, materia):
     }
 
     event = service.events().insert(calendarId="primary", body=event).execute()
+
+def get_event(dia, mes, materia):
+    SCOPES = ['https://www.googleapis.com/auth/calendar', 'https://www.googleapis.com/auth/calendar.events', 'https://www.googleapis.com/auth/calendar.readonly']
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('calendar', 'v3', credentials=creds)
+
+    except HttpError as error:
+        print('An error occurred: %s' % error)
+
+    event = service.events().list(calendarId="primary", q=materia).execute()
+    return event
