b831d253711e0da18d6948905fff63458ea11316
Lucas Peronico Barbotin <lucas.peronico@labs.dcx>
2022-11-21 10:55:17 -0300

    ajeitando um monte de erro besta

--- main.py
+++ main.py
@@ -1,87 +1,48 @@
+from kivy.app import App
+from kivy.properties import StringProperty
+from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
+import os
 
-from requests import Session, Request
-import requests
-from bs4 import BeautifulSoup
-from getpass import getpass
-from google_calendar import add_event
-from google_calendar import get_event
-import google_calendar
-import datetime
+from login import *
 
-def main(usuario, senha):
-    s = requests.Session()
+class Login(Screen):
+    def do_login(self, loginText, passwordText):
+        app = App.get_running_app()
 
-    r = s.get("https://sigaa.ufpb.br/sigaa/logon.jsf")
-    soup = BeautifulSoup(r.text, "lxml")
-    j_id = soup.find("input", {"id": "javax.faces.ViewState"})["value"]
+        app.username = loginText
+        app.password = passwordText
+        main(app.username, app.password)
 
-    # usuario = input("Seu usuario do sigaa: ")
-    # senha  = getpass("Senha: ")
+        app.config.read(app.get_application_config())
+        app.config.write()
 
-    dados = {
-        "form": "form",
-        "form:width":	"1920",
-        "form:height": "1080",
-        "form:login": usuario,
-        "form:senha": senha,
-        "form:entrar": "Entrar",
-        "javax.faces.ViewState":	j_id
-    }
+    def resetForm(self):
+        self.ids['login'].text = ""
+        self.ids['password'].text = ""
 
-    head =  {
+class LoginApp(App):
+    username = StringProperty(None)
+    password = StringProperty(None)
 
-        "Host": "sigaa.ufpb.br",
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Origin": "https://sigaa.ufpb.br",
-        "Connection": "keep-alive",
-        "Referer": "https://sigaa.ufpb.br/sigaa/logon.jsf",
-        "Upgrade-Insecure-Requests": "1",
-    }
+    def build(self):
+        manager = ScreenManager()
 
+        manager.add_widget(Login(name='login'))
 
-    s.headers = head
-    resp = s.post("https://sigaa.ufpb.br/sigaa/logon.jsf", data=dados)
+        return manager
 
-    lel = s.get("https://sigaa.ufpb.br/sigaa/portais/discente/beta/discente.jsf")
+    def get_application_config(self):
+        if(not self.username):
+            return super(LoginApp, self).get_application_config()
 
-    if (lel.url != "https://sigaa.ufpb.br/sigaa/portais/discente/beta/discente.jsf"):
-        print("Senha ou usuarion invalidos")
+        conf_directory = self.user_data_dir + '/' + self.username
 
-    else:
-        print("login feito com sucesso")
+        if(not os.path.exists(conf_directory)):
+            os.makedirs(conf_directory)
 
-        f = open("lel", "w")
-        f.write(lel.text)
+        return super(LoginApp, self).get_application_config(
+            '%s/config.cfg' % (conf_directory)
+        )
 
-        soup = BeautifulSoup(lel.text, "lxml")
-        turmas = soup.find("table", {"class": "minhas-turmas"})
-        trs = turmas.find_all("tr")
-
-        for i in range(1, len(trs), 1):
-            try:
-                materia_soup = BeautifulSoup(str(trs[i]), "lxml")
-                materia = materia_soup.find("a").text
-                info = materia_soup.find("div", {"class": "info"}).text
-                dia = 0
-                mes = 0
-                for j in range(len(info)):
-                    if (info[j] == "/"):
-                        dia = int(info[j-2]+info[j-1])
-                        mes = int(info[j+1]+info[j+2])
-
-                print(info)
-                print(str(dia)+"/"+str(mes))
-                print(materia)
-                if len(get_event(dia, mes, materia)["items"])>0:
-                    for events in get_event(dia, mes, materia)["items"]:
-                        if (events['start']['date'] != datetime.date.today().year+"-"+mes+"-"+dia and events['summary'] != materia):
-                            add_event(dia, mes, materia)
-                else:
-                    add_event(dia,mes,materia)
-
-
-            except:
-                pass
-
-
-# main()
+if __name__ == '__main__':
+    LoginApp().run()
